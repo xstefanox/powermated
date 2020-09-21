56d57232897100df5641213422883b6ed3a98277
Stefano Varesi <stefano.varesi@gmail.com>
2020-09-21 09:55:53 +0200

    avoid forking a process for every event and instead connect to pulseaudio daemon

--- powermated/__init__.py
+++ powermated/__init__.py
@@ -1,35 +1,30 @@
 from evdev import InputDevice, ecodes, list_devices
-import subprocess
 import sys
 import errno
 import logging
+import pulsectl
+from logging import DEBUG
 
-VOLUME_CMD = ['amixer', '-D', 'pulse', 'sset', 'Master']
-MUTE_TOGGLE = 0
+KEY_PRESS = 1
 
 log = logging.getLogger('powermated')
-log.setLevel(logging.DEBUG)
+log.setLevel(DEBUG)
 log.addHandler(logging.StreamHandler(sys.stdout))
 
 
 def find_device():
-    log.info("Searching for Griffin Powermate input device")
+    """
+    Search for the device representing the interface to the Griffin Powermate.
+
+    :return: A list containing the devices found, empty if no matching device is found.
+    """
+    log.info('Searching for Griffin Powermate input device')
     devices = [InputDevice(fn) for fn in list_devices()]
     for device in devices:
         if device.name.find('PowerMate') != -1:
-            log.info(f"Device found: ${device.name} (${device.phys})")
+            log.info('Device found: %s (%s)', device.name, device.phys)
             return [device.fn]
     return []
-
-
-def run_process(cmd):
-    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    stdout, stderr = process.communicate()
-
-    if stdout:
-        log.debug(stdout)
-    if stderr:
-        log.debug(stderr)
 
 
 def listen_on(device):
@@ -39,39 +34,40 @@ def listen_on(device):
     :param device: the name of the device to read
     """
 
-    log.debug('Listening on ' + device)
+    log.info('Listening on %s', device)
 
     try:
+        with pulsectl.Pulse('Griffin Powermate') as pulse:
 
-        for event in InputDevice(device).read_loop():
+            for event in InputDevice(device).read_loop():
 
-            # ignore synchronization events
-            if event.type == ecodes.EV_SYN:
-                continue
+                # ignore synchronization events
+                if event.type == ecodes.EV_SYN:
+                    continue
 
-            log.debug(f'Received event: ${event}')
+                # event action: toggle mute
+                if event.type == ecodes.EV_KEY:
 
-            # event action: mute
-            if event.type == ecodes.EV_KEY:
+                    if event.value == KEY_PRESS:
+                        for sink in pulse.sink_list():
+                            mute = not sink.mute
+                            log.debug('Received %s: %s', event, 'muting' if mute else 'unmuting')
+                            pulse.mute(sink, mute)
 
-                cmd = VOLUME_CMD[:]
-                cmd.append('toggle')
+                # event action: volume
+                elif event.type == ecodes.EV_REL:
 
-                if event.value == MUTE_TOGGLE:
-                    run_process(cmd)
+                    increase = event.value / 100
 
-            # event action: volume
-            elif event.type == ecodes.EV_REL:
+                    if log.isEnabledFor(DEBUG):
+                        log.debug('Received %s: volume %s', event, 'increase' if increase > 0 else 'decrease')
 
-                cmd = VOLUME_CMD[:]
-                cmd.append(str(abs(event.value)) + '%')
+                    for sink in pulse.sink_list():
+                        pulse.volume_change_all_chans(sink, increase)
 
-                if event.value > 0:
-                    cmd[-1] += '+'
                 else:
-                    cmd[-1] += '-'
 
-                run_process(cmd)
+                    log.debug('Ignoring unknown event type %s', event.type)
 
     except IOError as e:
 
